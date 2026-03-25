e45bcb0f013c4c15834e508b066bd95131ee80e8
lightclub-dev <devops@lightclubfitness.com>
2026-03-25 12:11:44 -0500

    Add Procfile

--- app.py
+++ app.py
@@ -201,4 +201,5 @@ def admin_logout():
     return redirect(url_for('public_results'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
